e50317b0179123b22f462bb71f71d02443b9c527
Igor Mozharovsky <igor.mozharovsky@gmail.com>
2016-11-27 21:58:50 +0200

    Add Redux Saga

--- app/store/configureStore.js
+++ app/store/configureStore.js
@@ -1,5 +1,6 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import createLogger from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
 import thunkMiddleware from 'redux-thunk';
 import createApiReducer from '../api/reducers';
 
@@ -8,17 +9,22 @@ const loggerMiddleware = createLogger();
 
 
 export default function configureStore(preloadedState) {
+  const sagaMiddleware = createSagaMiddleware();
   const api = createApiReducer();
   const rootReducer = combineReducers({
     api,
   });
 
-  return createStore(
-    rootReducer,
-    preloadedState,
-    applyMiddleware(
-      thunkMiddleware,
-      loggerMiddleware,
+  return {
+    ...createStore(
+      rootReducer,
+      preloadedState,
+      applyMiddleware(
+        thunkMiddleware,
+        sagaMiddleware,
+        loggerMiddleware,
+      ),
     ),
-  );
+    runSaga: sagaMiddleware.run,
+  };
 }
